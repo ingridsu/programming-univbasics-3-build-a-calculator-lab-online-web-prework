0581eef8a933a8886a94f22ae6b3f8498dac7e98
Ingrid sura <sura.ingrid@gmail.com>
2019-10-28 14:45:57 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,17 @@
 def addition(num1, num2)
-
+puts sum 
+a+b
 end
+addition(5,4)
 
 def subtraction(num1, num2)
-
+puts sub 
+a-b
+aubtraction(10,5)
 end
 
 def division(num1, num2)
-
+ puts num1+num2
 end
 
 def multiplication(num1, num2)
@@ -15,9 +19,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+puts math.sin(#{num1},#{num2})
+modulo(34,5)
 end
 
 def square_root(num)
-
+puts math.sqrt
+square_root(81)
 end
